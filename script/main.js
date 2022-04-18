41fc2244179f2caec5b052397fac0b15983d6282
unknown <chandravashi201@gmail.com>
2022-04-18 12:40:33 +0530

    Added FRNTR website

---
+++ script/main.js
@@ -0,0 +1,7 @@
+const distance=document.getElementById('header').offset().top;
+console.log(distance);
+// if((window.screenY)>distance){
+//     console.log('hii');
+// }else{
+//     console.log('hello');
+  
